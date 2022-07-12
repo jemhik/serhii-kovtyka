f0bea611de2fde7538c077884fff5db071fcb0a9
jemhik <sergij2003ua@gmail.com>
2022-07-12 12:33:15 +0300

    Updated App class

--- src/main/java/com/epam/spring/homework2/App.java
+++ src/main/java/com/epam/spring/homework2/App.java
@@ -2,15 +2,16 @@ package com.epam.spring.homework2;
 
 import com.epam.spring.homework2.beans.MainBean;
 import com.epam.spring.homework2.config.BeansConfig;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
-        String[] beanNames = context.getBeanDefinitionNames();
-        for (String beanName : beanNames) {
-            System.out.println(context.getBean(beanName));
-        }
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        ((ConfigurableApplicationContext) context).close();
     }
 }
